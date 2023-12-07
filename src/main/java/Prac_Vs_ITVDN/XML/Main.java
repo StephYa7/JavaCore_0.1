5c2bb00597837867efb84377605ab03391fb46c2
StephYa7 <1000p@mail.ru>
2023-12-07 23:04:08 +0700

    added XML parser from ITDVN

--- src/main/java/Prac_Vs_ITVDN/XML/Main.java
+++ src/main/java/Prac_Vs_ITVDN/XML/Main.java
@@ -1,4 +1,13 @@
 package Prac_Vs_ITVDN.XML;
 
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        XMLParser xml = new XMLParser("src/main/java/Prac_Vs_ITVDN/XML/1.xml");
+        String value = xml.get("catalog/book/author");
+        System.out.println(value);
+
+    }
 }
---
+++ src/main/java/Prac_Vs_ITVDN/XML/XMLParser.java
@@ -0,0 +1,55 @@
+package Prac_Vs_ITVDN.XML;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+
+public class XMLParser {
+    private String xml;
+
+    public XMLParser(String path) {
+        byte[] buf = null;
+        try (RandomAccessFile file = new RandomAccessFile(path, "r")) {
+            buf = new byte[(int) file.length()];
+            file.read(buf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            xml = new String(buf, "US-ASCII");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    // Метод определения "catalog/book/author"
+    public String get(String path) {
+        // Разбиваем /
+        String[] parts = path.split("/");
+        String s_xml = xml;
+
+        for (String s : parts) {
+            String toFind = "<" + s + ">";
+            // start начинается с 1 символа найденного тега
+            int start = s_xml.indexOf(toFind);
+            if (start < 0) {
+                return null;
+            } else {
+                start += toFind.length();
+            }
+
+            // Находим закрывающийся тег
+            int end = s_xml.lastIndexOf("</" + s + ">");
+            if (end < 0) {
+                return null;
+            }
+            // Вырезаем найденную строку
+            s_xml = s_xml.substring(start, end);
+        }
+
+        return s_xml;
+    }
+    
+}
