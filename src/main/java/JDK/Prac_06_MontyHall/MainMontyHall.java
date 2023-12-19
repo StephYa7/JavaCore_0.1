4465a22f0f6107d6db9f80cd592a0c66577d37b1
StephYa7 <1000p@mail.ru>
2023-12-19 19:38:56 +0700

    solved monty hall task without map

--- src/main/java/JDK/Prac_06_MontyHall/MainMontyHall.java
+++ src/main/java/JDK/Prac_06_MontyHall/MainMontyHall.java
@@ -1,41 +1,44 @@
 package JDK.Prac_06_MontyHall;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class MainMontyHall {
     public static void main(String[] arg) {
-
-
         double win = 0;
-        double winWithoutShift = 0;
         double winAfterShift = 0;
         double lose = 0;
-        double loseWithoutShift = 0;
         double loseAfterShift = 0;
-        for (int i = 0; i < 5000; i++) {
-            String[] doors = createMontyHallDoors();
+        for (int i = 0; i < 100; i++) {
+            List<String> doors = createMontyHallDoors();
             int choice = new Random().nextInt(3);
-            if (doors[choice].equals("car")) win++;
-            else lose++;
-
-
+            if (doors.get(choice).equals("car")) {
+                win++;
+                doors.remove(choice);
+            } else lose++;
+            doors.remove("goat");        // Если меняем выбор, тот удаляем из пула
+            if (doors.contains("car")) {   // дверей любую за которой есть козел,
+                winAfterShift++;          // это та дверь которую нам открывает ведущий
+            } else loseAfterShift++;     // перед предложением изменить выбор.
 
 
         }
-
         double result = win / (win + lose) * 100;
+        double resultAfterShift = winAfterShift / (winAfterShift + loseAfterShift) * 100;
         System.out.println(result);
-
-
+        System.out.printf("Процент побед, без смены выбора - %f%%\n", result);
+        System.out.printf("Процент побед, при смене выбора - %s%%", resultAfterShift);
     }
 
-    private static String[] createMontyHallDoors() {
-        String[] threeDoors = {"goat", "goat", "goat"};
+    private static List<String> createMontyHallDoors() {
+        List<String> threeDoors = new ArrayList<>();
+        Collections.addAll(threeDoors, "goat", "goat", "goat");
         int carIndex = new Random().nextInt(3);
-        threeDoors[carIndex] = "car";
+        threeDoors.set(carIndex, "car");
         return threeDoors;
     }
+
 
 }
--- src/main/java/Test/MyDraft.java
+++ src/main/java/Test/MyDraft.java
@@ -1,32 +1,7 @@
 package Test;
 
-import java.util.Random;
-
 class MyDraft {
     public static void main(String[] arg) {
 
-
-        double win = 0;
-        double lose = 0;
-        for (int i = 0; i < 5000; i++) {
-            boolean[] doors = createMontyHallDoors();
-            int choice = new Random().nextInt(3);
-//            if (doors[choice]) win++;
-//            else lose++;
-
-
-        }
-
-        double result = win / (win + lose) * 100;
-        System.out.println(result);
-
-
-    }
-
-    private static boolean[] createMontyHallDoors() {
-        boolean[] threeDoors = {false, false, false};
-        int carIndex = new Random().nextInt(3);
-        threeDoors[carIndex] = true;
-        return threeDoors;
     }
 }
